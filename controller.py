5f5a1c18bc41e71dcada3b5f004650a565b3ad01
cabezueloo <angelcabezuelovert@gmail.com>
2025-02-22 22:46:26 +0100

    Stats of who followers, following and saw history get it

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 from model import SeleniumInstagram
 from view import View
 
+STATS : str = 'stats/'
+
 class Controller_Instagram:
 
     def __init__(self, seleniumInstagram:SeleniumInstagram,view:View):
@@ -8,9 +10,40 @@ class Controller_Instagram:
         self.viewController = view
         self.listaFollowers = []
         self.listaFollowing = []
+        self.listaViews = []
     
     def controllerStartAnalyse(self) : self.seleniumInstragramController.modelStartAnalyse()
 
-    def show(self):
+    def show_stats(self):
+        
         print(f"Tamagno followers -> {len(self.listaFollowers)}")
         print(f"Tamagno following -> {len(self.listaFollowing)}")
+        print(f"Tamagno views -> {len(self.listaViews)}")
+
+    def users_saw_history_and_not_follow_your_account(self):
+        
+        route = STATS+"usersSawHistoryNotFollowYourAccount.txt"
+        f = open(file=route+"",mode='w')
+
+        for user in self.listaViews:
+            if not user in self.listaFollowers:
+                #print(f"El usuario {user} no te sigue y te ha visto la historia")
+                f.write(f"El usuario {user} no te sigue y te ha visto la historia\n")
+        
+        f.close()
+    
+    def users_not_follow_back(self):
+
+        route = STATS+"usersNotFollowBack.txt"
+        f = open(route,mode='w')
+
+        for follow in self.listaFollowing:
+            if not follow in self.listaFollowers:
+                f.write(f"El usuario {follow} no te sigue de vuelta\n")
+
+        
+
+    
+
+    
+
--- model.py
+++ model.py
@@ -12,6 +12,11 @@ FOLLOWING : str = "/following/"
 ROUTE_COOKIES = "cookie_login/"
 USERNANE = "angeel.bjj"
 
+FRAME_FOLLOWERS_FOLLOWING = "'xyi19xy x1ccrb07 xtf3nb5 x1pc53ja x1lliihq x1iyjqo2 xs83m0k xz65tgg x1rife3k x1n2onr6'"
+NAME_FOLLOWERS_FOLLOGIN = "'_ap3a _aaco _aacw _aacx _aad7 _aade'"
+
+FRAME_VIWED_HISTORY = "'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 xw2csxc x1odjw0f x1iyjqo2 x2lwn1j xeuugli xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1'"
+NAME_VIWED_HISTORY = "'x1lliihq x1plvlek xryxfnj x1n2onr6 x1ji0vk5 x18bv5gf x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj'"
 class SeleniumInstagram:
 
     def __init__(self):
@@ -19,7 +24,7 @@ class SeleniumInstagram:
         self.account = USERNANE
         self.url = BASE_URL+self.account+"/"
         self.followers = list()
-
+        self.views      = set(())
         self.driver.implicitly_wait(3.0) # Wait 3 seconds into get a element
         self.login()
 
@@ -47,23 +52,17 @@ class SeleniumInstagram:
         #Open Followers 
         elementFollowersToClick = self.driver.find_element(By.XPATH, f"//a[@href='/{USERNANE}{search}']")
         elementFollowersToClick.click()
-        actualName = ""
         
-        frame  = self.driver.find_element(By.XPATH,"//div[@class='xyi19xy x1ccrb07 xtf3nb5 x1pc53ja x1lliihq x1iyjqo2 xs83m0k xz65tgg x1rife3k x1n2onr6']")
+        frame  = self.driver.find_element(By.XPATH,f"//div[@class={FRAME_FOLLOWERS_FOLLOWING}]")
         scroll_origin = ScrollOrigin.from_element(frame)
-        last_y = -1
         tryAagain = True
 
         lastElementName = ""
         
         while True:
             
-            names = self.driver.find_elements(By.XPATH, "//*[@class='_ap3a _aaco _aacw _aacx _aad7 _aade']")
+            names = self.driver.find_elements(By.XPATH, f"//*[@class={NAME_FOLLOWERS_FOLLOGIN}]")
             
-
-           # print(names[-1].rect)
-            #print(names[-1].rect['y'])
-           # print(print(names[-1].text))
 
             ActionChains(self.driver)\
             .scroll_from_origin(scroll_origin,0,int(names[-1].rect['y']))\
@@ -81,22 +80,57 @@ class SeleniumInstagram:
                 break
 
         #Fuera while true
-        names = self.driver.find_elements(By.XPATH, "//*[@class='_ap3a _aaco _aacw _aacx _aad7 _aade']")
+        names = self.driver.find_elements(By.XPATH, f"//*[@class={NAME_FOLLOWERS_FOLLOGIN}]")
 
         for name in names:
             self.followers.append(name.text)
 
-        #print(self.followers)
-       # print(f"Tamagno -> {len(self.followers)}")
-             
-        
-        
        
-
-
-
+             
+    def seeVisitedHistory(self):
         
         
+        self.driver.get("https://www.instagram.com/stories/archive/18299908186225234/?initial_media_id=3572457676619689748")
+
+        element = self.driver.find_element(By.XPATH, "//span[@class='xfungia x1yxbuor x19qstwj x1yrs1ss']")
+        element.click()
+        time.sleep(4)
+
+        frame  = self.driver.find_element(By.XPATH,f"//div[@class={FRAME_VIWED_HISTORY}]")
+        scroll_origin = ScrollOrigin.from_element(frame)
+        lastElementName=""
+        tryAagain = True
 
 
+        while True:
+            
+            names = self.driver.find_elements(By.XPATH, f"//span[@class={NAME_VIWED_HISTORY}]")
+            
+            for name in names:
+                #print(name.text)
+                self.views.add(name.text)
+           # print(names[-1].rect)
+            #print(names[-1].rect['y'])
+           # print(print(names[-1].text))
 
+            ActionChains(self.driver)\
+            .scroll_from_origin(scroll_origin,0,int(names[-1].rect['y']))\
+            .perform()
+
+            time.sleep(0.5)
+            
+            if lastElementName != names[-1].text:
+                lastElementName = names[-1].text
+                tryAagain = True
+            else:
+                if tryAagain:
+                    time.sleep(1) 
+                    tryAagain = False       
+                    continue
+                break
+        
+        #Fuera while true
+        names = self.driver.find_elements(By.XPATH, f"//span[@class={NAME_VIWED_HISTORY}]")
+
+        for name in names:
+            self.views.add(name.text)
